0bdfee1dab6ec43db7e0fc456888b42ff98b6110
Sabrina Tonom <sabrinatonom@gmail.com>
2020-10-26 21:03:14 -0300

    Estudo de Caso
    Arquivos relacionados ao Estudo de Caso, com divisão de tarefas.

---
+++ Estudo de Caso/Sabrina Tonom.mm
@@ -0,0 +1,280 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1603402209765" ID="ID_1150360531" MODIFIED="1603404148225">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><i><font color="#3333ff">Sabrina Tonom</font></i></b>
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1603402260065" ID="ID_710119389" MODIFIED="1603641015382" POSITION="right" TEXT="Pessoal">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="freemind_butterfly"/>
+<node COLOR="#00b439" CREATED="1603402277713" FOLDED="true" ID="ID_962616866" MODIFIED="1603640981464" TEXT="Caracter&#xed;sticas F&#xed;sicas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1603402220886" ID="ID_1446476103" MODIFIED="1603404148214" TEXT="Pode me chamar de S&#xe1;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1603402235017" ID="ID_502183227" MODIFIED="1603640980105" TEXT="Tenho 40 anos">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1603402238529" ID="ID_503860296" MODIFIED="1603404148215" TEXT="Sou bem alta, rsrs 1,77 m">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1603402296614" ID="ID_1604570881" MODIFIED="1603565804009" TEXT="67 kg">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1603402316569" ID="ID_531350513" MODIFIED="1603565811983" TEXT="cor de pele branca">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1603402319063" ID="ID_1567547992" MODIFIED="1603565816413" TEXT="cabelo loiro escuro e enrolado">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1603402361958" ID="ID_998863642" MODIFIED="1603404148215" TEXT="olhos castanhos escuros">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1603402368396" ID="ID_445154269" MODIFIED="1603404148215" TEXT="cal&#xe7;o 40 (tb para meu tamanho n&#xe3;o poderia ser menos)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1603404221954" FOLDED="true" ID="ID_1379096925" MODIFIED="1603565901237" TEXT="Informa&#xe7;&#xf5;es Pessoais">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1603404245501" ID="ID_791524606" MODIFIED="1603565834509" TEXT="Nasci em SP">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1603404271609" ID="ID_562877989" MODIFIED="1603404310709" TEXT="Morei do zero aos 23 anos em Guarulhos"/>
+<node COLOR="#111111" CREATED="1603404313607" ID="ID_1522823769" MODIFIED="1603565875135" TEXT="Dos 24 aos 28 em S&#xe3;o Paulo">
+<node COLOR="#111111" CREATED="1603565867211" ID="ID_1586279415" MODIFIED="1603565870661" TEXT="Congonhas"/>
+</node>
+<node COLOR="#111111" CREATED="1603404323835" ID="ID_1892778565" MODIFIED="1603404335288" TEXT="Dos 28 aos 29 em Mogi das Cruzes"/>
+<node COLOR="#111111" CREATED="1603404335694" ID="ID_834238389" MODIFIED="1603404343312" TEXT="Dos 30 aos 34 em Guarulhos"/>
+<node COLOR="#111111" CREATED="1603404343757" ID="ID_790467077" MODIFIED="1603565881520" TEXT="Dos 35 a atual em S&#xe3;o Paulo">
+<node COLOR="#111111" CREATED="1603565882960" ID="ID_1880308801" MODIFIED="1603565885802" TEXT="Interlagos"/>
+<node COLOR="#111111" CREATED="1603565886241" ID="ID_1801162162" MODIFIED="1603565891531" TEXT="Bela Vista"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1603404470738" ID="ID_1648717112" MODIFIED="1603565893166" TEXT="Minha fam&#xed;lia">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1603404491300" ID="ID_586732911" MODIFIED="1603404495768" TEXT="M&#xe3;e "/>
+<node COLOR="#111111" CREATED="1603404496088" ID="ID_768120851" MODIFIED="1603404498329" TEXT="irm&#xe3;"/>
+<node COLOR="#111111" CREATED="1603404499195" ID="ID_18379130" MODIFIED="1603404501911" TEXT="Namorido">
+<node COLOR="#111111" CREATED="1603404449967" ID="ID_986204970" MODIFIED="1603404455522" TEXT="Juntos h&#xe1; 8 anos"/>
+<node COLOR="#111111" CREATED="1603404392327" ID="ID_1466646403" MODIFIED="1603404541068" TEXT="sem filhos"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1603402433611" FOLDED="true" ID="ID_70206287" MODIFIED="1603566084115" TEXT="Forma&#xe7;&#xe3;o">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1603402465863" ID="ID_623088294" MODIFIED="1603404148216">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      T&#233;cnico em Processamento de Dados
+    </p>
+    <p style="text-align: center">
+      &#160;(em 1997 era este o nome para TI)
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1603402520048" ID="ID_1927290047" MODIFIED="1603404148216">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Gradu&#231;&#227;o em Qualidade em 2001,
+    </p>
+    <p style="text-align: center">
+      &#160;sim sou formada h&#225; 19 anos
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1603403748336" ID="ID_886250155" MODIFIED="1603565934886" TEXT="Aperfei&#xe7;oamento em Estat&#xed;stica em 2005 e 2012">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1603402575628" ID="ID_766221894" MODIFIED="1603404148216" TEXT="P&#xf3;s Gradua&#xe7;&#xe3;o em Gest&#xe3;o Industrial em 2011">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1603402598415" FOLDED="true" ID="ID_811741687" MODIFIED="1603566022481" TEXT="Cursos livres">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1603402625562" ID="ID_1966308980" MODIFIED="1603404148217" TEXT="Marketing Digital"/>
+<node COLOR="#111111" CREATED="1603402635056" ID="ID_836666826" MODIFIED="1603404148217" TEXT="Googe Adwords">
+<node COLOR="#111111" CREATED="1603565960124" ID="ID_1133121239" MODIFIED="1603565973122" TEXT="Dei aula em uma ONG: CTC Digital"/>
+</node>
+<node COLOR="#111111" CREATED="1603402639093" ID="ID_577141550" MODIFIED="1603404148217" TEXT="Analytics"/>
+<node COLOR="#111111" CREATED="1603402641600" ID="ID_1273795749" MODIFIED="1603404148217" TEXT="SEO"/>
+<node COLOR="#111111" CREATED="1603403763827" ID="ID_383071264" MODIFIED="1603404148217" TEXT="Matem&#xe1;tica Aplicada"/>
+<node COLOR="#111111" CREATED="1603402672528" ID="ID_1232700864" MODIFIED="1603404148217" TEXT="CTFL - Iterasys"/>
+<node COLOR="#111111" CREATED="1603402674705" ID="ID_951488440" MODIFIED="1603404148217" TEXT="CTFL-AT - Iterasys"/>
+<node COLOR="#111111" CREATED="1603402648060" ID="ID_112673002" MODIFIED="1603404148217" TEXT="FTS  - Iterasys">
+<icon BUILTIN="ksmiletris"/>
+</node>
+<node COLOR="#111111" CREATED="1603402667594" ID="ID_791204256" MODIFIED="1603404148217" TEXT="TSPI - Julio de Lima"/>
+<node COLOR="#111111" CREATED="1603402975683" ID="ID_1570247086" MODIFIED="1603404148218" TEXT="Reaprendizagem Criativa - Murilo Gan"/>
+<node COLOR="#111111" CREATED="1603402914670" ID="ID_598312925" MODIFIED="1603404148218" TEXT="Entre outros milhares, AMO estudar"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1603402443861" FOLDED="true" ID="ID_515939846" MODIFIED="1603641016451" TEXT="Robbies">
+<edge STYLE="bezier" WIDTH="thin"/>
+<arrowlink DESTINATION="ID_515939846" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1328178694" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_515939846" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1328178694" SOURCE="ID_515939846" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1603402761546" ID="ID_384428438" MODIFIED="1603566038510" TEXT="Corrida">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1603566027523" ID="ID_66154662" MODIFIED="1603566033706" TEXT="4 Meia Maratonas">
+<node COLOR="#111111" CREATED="1603640996872" ID="ID_129291399" MODIFIED="1603641008167" TEXT="Em 2020 ia fazer minha 1&#xaa; Maratona, mas..."/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1603402773054" ID="ID_171505245" MODIFIED="1603404148219" TEXT="Pedal">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1603566040388" ID="ID_1796967728" MODIFIED="1603566055764" TEXT="Passeios leves, uns 40 km"/>
+</node>
+<node COLOR="#990000" CREATED="1603402786993" ID="ID_1040610158" MODIFIED="1603404148219" TEXT="Trilha">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1603402819768" ID="ID_1749659196" MODIFIED="1603404148219">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Qualquer atividade ao ar livre
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1603402253367" ID="ID_82550615" MODIFIED="1603404593096" POSITION="left" TEXT="Profissional">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="bookmark"/>
+<node COLOR="#00b439" CREATED="1603403924307" FOLDED="true" ID="ID_1587121607" MODIFIED="1603565728779" TEXT="Carreira">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1603403063646" ID="ID_1836043285" MODIFIED="1603404148220" TEXT="1997: Metal&#xfa;rgica">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1603403605324" ID="ID_195409714" MODIFIED="1603404148220" TEXT="Est&#xe1;gio em Processamento de Dados"/>
+</node>
+<node COLOR="#990000" CREATED="1603403086821" ID="ID_1024045282" MODIFIED="1603404560733">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      De 1998 a 1999
+    </p>
+    <p style="text-align: center">
+      Comissaria de Bordo em Guarulhos
+    </p>
+    <p style="text-align: center">
+      Empresa que faz os alimentos servidos no avi&#227;o
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1603403619573" ID="ID_1379267649" MODIFIED="1603404148221" TEXT="Planejamento de Produ&#xe7;&#xe3;o"/>
+</node>
+<node COLOR="#990000" CREATED="1603403137977" ID="ID_1911022145" MODIFIED="1603404148222" TEXT="2000 a 2001 2&#xba; Ind&#xfa;stria Qu&#xed;mica">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1603403628238" ID="ID_134587336" MODIFIED="1603404148222" TEXT="Est&#xe1;gio na &#xe1;rea da Qualidade"/>
+</node>
+<node COLOR="#990000" CREATED="1603403153873" ID="ID_274390906" MODIFIED="1603404148223">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      2002 a 2005 Comissaria de Bordo em Congonhas
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1603403733868" ID="ID_1782191710" MODIFIED="1603404148223" TEXT="Planejamento de Produ&#xe7;&#xe3;o"/>
+</node>
+<node COLOR="#990000" CREATED="1603403370513" ID="ID_1510044031" MODIFIED="1603565540464" TEXT="2005 a 2014 Consultoria em Gest&#xe3;o Empresarial">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1603403786049" ID="ID_1466387916" MODIFIED="1603565574899" TEXT="Consultora de Implanta&#xe7;&#xe3;o da NBR ISO 9001"/>
+<node COLOR="#111111" CREATED="1603403805887" ID="ID_1164802013" MODIFIED="1603565592639" TEXT="&#xc1;rea Administrativa / Financeira / Planejamento"/>
+</node>
+<node COLOR="#990000" CREATED="1603403399758" ID="ID_1788853658" MODIFIED="1603404148223">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      2012 a atual em uma consultoria de TI e Digital
+    </p>
+    <p>
+      sim, por 2 anos tive 2 empregos, n&#227;o sei como sobrevivi rsrs
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1603403823253" ID="ID_122189213" MODIFIED="1603404148223" TEXT="RH"/>
+<node COLOR="#111111" CREATED="1603403828237" ID="ID_1194232969" MODIFIED="1603404148223" TEXT="Financeiro"/>
+<node COLOR="#111111" CREATED="1603403833714" ID="ID_463681158" MODIFIED="1603404148223" TEXT="Comercial"/>
+<node COLOR="#111111" CREATED="1603403836955" ID="ID_1497388339" MODIFIED="1603404148224" TEXT="Marketing Digital"/>
+<node COLOR="#111111" CREATED="1603403892574" ID="ID_1134871300" MODIFIED="1603404148224" TEXT="Gest&#xe3;o de eCommece"/>
+<node COLOR="#111111" CREATED="1603403841772" ID="ID_1219367060" MODIFIED="1603565676461">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      e finalmente para minha alegria: <b>Testes</b>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1603403946369" FOLDED="true" ID="ID_303503849" MODIFIED="1603565771087" TEXT="Jornada de Testes">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1603404056273" ID="ID_747401118" MODIFIED="1603404148224" TEXT="Projetos">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1603403987232" ID="ID_1804165686" MODIFIED="1603404148224" TEXT="Quizzes e experi&#xea;ncia on-line">
+<node COLOR="#111111" CREATED="1603452831671" ID="ID_899875480" MODIFIED="1603452843072" TEXT="Testes funcionais (UI/UX)"/>
+</node>
+<node COLOR="#111111" CREATED="1603403952188" ID="ID_284983360" MODIFIED="1603404580712" TEXT="PDV e AV">
+<node COLOR="#111111" CREATED="1603452847340" ID="ID_26676986" MODIFIED="1603452855695" TEXT="Testes funcionais"/>
+</node>
+<node COLOR="#111111" CREATED="1603403967219" ID="ID_1962364642" MODIFIED="1603404583451" TEXT="WMS">
+<node COLOR="#111111" CREATED="1603452858100" ID="ID_1153205296" MODIFIED="1603452862254" TEXT="Testes funcionais"/>
+<node COLOR="#111111" CREATED="1603452862766" ID="ID_1267715952" MODIFIED="1603452866791" TEXT="Testes de API"/>
+</node>
+<node COLOR="#111111" CREATED="1603404075989" ID="ID_1827675400" MODIFIED="1603404148225" TEXT="eCommerce">
+<node COLOR="#111111" CREATED="1603452869737" ID="ID_1171889704" MODIFIED="1603452874178" TEXT="Testes funcionais"/>
+<node COLOR="#111111" CREATED="1603452874783" ID="ID_1508504216" MODIFIED="1603452880267" TEXT="Testes de Carga"/>
+<node COLOR="#111111" CREATED="1603452881576" ID="ID_1800768174" MODIFIED="1603452900684" TEXT="Testes de Stress"/>
+<node COLOR="#111111" CREATED="1603452908508" ID="ID_1768589284" MODIFIED="1603452918031" TEXT="Testes de Integra&#xe7;&#xe3;o"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ Estudo de Caso/caso_de_teste_mapa_mental.mm
@@ -0,0 +1,124 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1603645332332" ID="ID_1839964198" MODIFIED="1603646345650" TEXT="App Hora de Comprar">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1603645877839" ID="ID_898726292" MODIFIED="1603646352727" POSITION="right" TEXT="Criar Lista de Compra">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1603645899807" ID="ID_807158805" MODIFIED="1603646345634" TEXT="Abrir o App Hora de Comprar&#x9;&#x9;&#x9;&#x9;&#x9;&#x9;&#x9;&#x9;&#x9;&#x9;&#x9;&#x9;&#x9;&#x9;&#x9;&#x9; ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node COLOR="#00b439" CREATED="1603645964740" ID="ID_1888153339" MODIFIED="1603646345634" TEXT="Clicar na foto do usu&#xe1;rio Stella">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node COLOR="#00b439" CREATED="1603645959363" ID="ID_340799310" MODIFIED="1603646345634" TEXT="Clicar na categoria &quot;Nossa Horta&quot;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-3"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646004654" ID="ID_1183796578" MODIFIED="1603646345634" TEXT="Clicar na sub-categoria &quot;Frutas&quot;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-4"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646016785" ID="ID_765208870" MODIFIED="1603646345634" TEXT="Na coluna comprar, em banana digitar 6">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-5"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646019821" ID="ID_807286453" MODIFIED="1603646345634" TEXT="Pressionar Enter">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-6"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646021167" ID="ID_1099679239" MODIFIED="1603646345634" TEXT="Na coluna comprar, em mam&#xe3;o digitar 1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-7"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646022634" ID="ID_1554110954" MODIFIED="1603646345634" TEXT="Pressionar Enter">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-8"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646023811" ID="ID_431417253" MODIFIED="1603646345634" TEXT="Na coluna comprar, em manga digitar 2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-9"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646025330" ID="ID_1665373553" MODIFIED="1603646345634" TEXT="Pressionar Enter">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-0"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646027832" ID="ID_42804423" MODIFIED="1603646345634" TEXT="Clicar em Salvar">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646030391" ID="ID_566406505" MODIFIED="1603646345634" TEXT="Clicar no &#xcd;cone do Menu para retornar a p&#xe1;gina de categoria">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646032138" ID="ID_905008614" MODIFIED="1603646345634" TEXT="Clicar na categoria &quot;Ovos e Carnes&quot;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-3"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646039319" ID="ID_541356165" MODIFIED="1603646345634" TEXT="Clicar na sub-categoria &quot;Carne&quot;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-4"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646042161" ID="ID_274882564" MODIFIED="1603646345634" TEXT="Na coluna comprar, em filet mignon digitar 1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-5"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646045149" ID="ID_309962293" MODIFIED="1603646345634" TEXT="Pressionar Enter">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-6"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646048307" ID="ID_227944142" MODIFIED="1603646345634" TEXT="Na coluna comprar, em hamburguer digitar 2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-7"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646051110" ID="ID_1520163496" MODIFIED="1603646345650" TEXT="Pressionar Enter">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-8"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646053182" ID="ID_1753654274" MODIFIED="1603646345650" TEXT="Clicar em Criar Lista">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-9"/>
+</node>
+<node COLOR="#00b439" CREATED="1603646383994" ID="ID_1270882587" MODIFIED="1603646402460" TEXT="Resultado Esperado">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="wizard"/>
+<node COLOR="#990000" CREATED="1603646389608" ID="ID_856093851" MODIFIED="1603646592960" TEXT="Lista de Compras Criada">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
